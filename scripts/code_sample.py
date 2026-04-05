6f15d3edf55b1333a4f64012c0ef80a357285d89
srivar28 <158322579+srivar28@users.noreply.github.com>
2026-04-05 18:03:55 -0400

    Delete scripts/code_sample.py

--- scripts/code_sample.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open('../data/case_with_all_sources_with_companion_cases_tag.jsonl', 'rb') as f:
-    # Just read the first N bytes to see structure
-    print(f.read(3000).decode('utf-8', errors='replace'))
